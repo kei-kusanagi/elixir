3b2fa6047f5b2ee50ae9d29ace5913c42b10a5b5
kei_kusanagi <kei_kusanagi@outlook.comi>
2022-04-07 18:00:36 -0500

    pipe, no le entendi

---
+++ Introduction to Modules/06pipe.exs
@@ -0,0 +1,18 @@
+defmodule PipeTest do
+  def square(x), do: x * x
+
+  def sum(l, start \\ 0) do
+    start + Enum.sum(l)
+  end
+
+  # tl
+
+  def sst(the_list) do
+    the_list
+    |> tl
+    |> sum(2)
+    |> square
+  end
+end
+
+IO.puts PipeTest.sst([1, 2, 5])
